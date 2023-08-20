4b0b6d84b67dd6ba610331c4556b7bb3d20a41c7
cbysal <1187588638@qq.com>
2023-08-20 11:15:28 +0800

    enhance CSE

--- src/main/java/compile/codegen/virgen/pass/CommonSubexpressionElimination.java
+++ src/main/java/compile/codegen/virgen/pass/CommonSubexpressionElimination.java
@@ -4,16 +4,15 @@ import compile.codegen.virgen.Block;
 import compile.codegen.virgen.VReg;
 import compile.codegen.virgen.VirtualFunction;
 import compile.codegen.virgen.vir.*;
+import compile.symbol.FuncSymbol;
 import compile.symbol.GlobalSymbol;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CommonSubexpressionElimination extends Pass {
-    private record Item(Item.Op op, VIRItem source1, VIRItem source2) {
+    private record Item(Item.Op op, FuncSymbol func, List<VIRItem> sources) {
         public enum Op {
-            ADD, SUB, MUL, DIV, MOD, EQ, NE, GE, GT, LE, LT, F2I, I2F, NEG, L_NOT, ABS
+            ADD, SUB, MUL, DIV, MOD, EQ, NE, GE, GT, LE, LT, F2I, I2F, NEG, L_NOT, ABS, CALL
         }
     }
 
@@ -23,6 +22,43 @@ public class CommonSubexpressionElimination extends Pass {
 
     @Override
     public boolean run() {
+        Set<FuncSymbol> pureFuncs = new HashSet<>();
+        for (VirtualFunction func : funcs.values())
+            pureFuncs.add(func.getSymbol());
+        for (VirtualFunction func : funcs.values()) {
+            boolean isPure = true;
+            for (Block block : func.getBlocks()) {
+                for (VIR ir : block) {
+                    if (ir instanceof CallVIR callVIR) {
+                        if (callVIR.func.getName().equals("memset"))
+                            continue;
+                        if (!pureFuncs.contains(callVIR.func)) {
+                            isPure = false;
+                            break;
+                        }
+                        continue;
+                    }
+                    if (ir instanceof LoadVIR loadVIR) {
+                        if (loadVIR.symbol instanceof GlobalSymbol) {
+                            isPure = false;
+                            break;
+                        }
+                        continue;
+                    }
+                    if (ir instanceof StoreVIR storeVIR) {
+                        if (storeVIR.symbol instanceof GlobalSymbol) {
+                            isPure = false;
+                            break;
+                        }
+                        continue;
+                    }
+                }
+                if (!isPure)
+                    break;
+            }
+            if (!isPure)
+                pureFuncs.remove(func.getSymbol());
+        }
         for (VirtualFunction func : funcs.values()) {
             for (Block block : func.getBlocks()) {
                 Map<Item, VReg> replaceMap = new HashMap<>();
@@ -42,12 +78,25 @@ public class CommonSubexpressionElimination extends Pass {
                             case GT -> Item.Op.GT;
                             case LE -> Item.Op.LE;
                             case LT -> Item.Op.LT;
-                        }, binaryVIR.left, binaryVIR.right);
+                        }, null, List.of(binaryVIR.left, binaryVIR.right));
                         if (replaceMap.containsKey(item)) {
                             block.set(i, new MovVIR(target, replaceMap.get(item)));
                             continue;
                         }
                         replaceMap.put(item, target);
+                        continue;
+                    }
+                    if (ir instanceof CallVIR callVIR) {
+                        if (pureFuncs.contains(callVIR.func) && callVIR.target != null) {
+                            VReg target = callVIR.target;
+                            Item item = new Item(Item.Op.CALL, callVIR.func, callVIR.params);
+                            if (replaceMap.containsKey(item)) {
+                                block.set(i, new MovVIR(target, replaceMap.get(item)));
+                                continue;
+                            }
+                            replaceMap.put(item, target);
+                            continue;
+                        }
                         continue;
                     }
                     if (ir instanceof UnaryVIR unaryVIR) {
@@ -58,7 +107,7 @@ public class CommonSubexpressionElimination extends Pass {
                             case NEG -> Item.Op.NEG;
                             case L_NOT -> Item.Op.L_NOT;
                             case ABS -> Item.Op.ABS;
-                        }, unaryVIR.source, null);
+                        }, null, List.of(unaryVIR.source));
                         if (replaceMap.containsKey(item)) {
                             block.set(i, new MovVIR(target, replaceMap.get(item)));
                             continue;
